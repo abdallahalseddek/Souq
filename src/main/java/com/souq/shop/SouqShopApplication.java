f3a2ed31ab8466f2df5fdcdd337b035c8eebc276
abdallahalseddek <abdallahalseddek@gmail.com>
2021-07-25 02:07:32 +0200

    Create project.close #1.

---
+++ src/main/java/com/souq/shop/SouqShopApplication.java
@@ -0,0 +1,13 @@
+package com.souq.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SouqShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SouqShopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/souq/shop/SouqShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.souq.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SouqShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
