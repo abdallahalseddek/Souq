9444c84397db3a226fdfbb798a6b78b6b17eddb7
abdallahalseddek <abdallahalseddek@gmail.com>
2021-07-27 20:14:58 +0200

    category api .close #5.

---
+++ src/main/java/com/souq/shop/controller/CategoryController.java
@@ -0,0 +1,28 @@
+package com.souq.shop.controller;
+
+import com.souq.shop.entity.Category;
+import com.souq.shop.repository.CategoryRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/category")
+@AllArgsConstructor
+public class CategoryController {
+    private final CategoryRepository categoryRepository;
+
+    @PostMapping
+    public ResponseEntity<Category> addCategory(@RequestBody Category category){
+        Category savedCategory = categoryRepository.save(category);
+        return new ResponseEntity<>(savedCategory, HttpStatus.CREATED);
+    }
+    @GetMapping
+    public ResponseEntity<List<Category>> allCategories(){
+        List<Category> categoryList = categoryRepository.findAll();
+        return new ResponseEntity<>(categoryList, HttpStatus.FOUND);
+    }
+}
